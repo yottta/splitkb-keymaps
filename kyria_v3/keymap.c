9e381994bd9baa9fdd68f5a962f75baf6e00fd86
yottta <andreic9203@gmail.com>
2024-04-26 17:28:04 +0300

    some changes for kyria

--- kyria_v3/keymap.c
+++ kyria_v3/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_TAB, KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN , KC_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_MUTE,     _______, KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_GRV,
-                                KC_LGUI,  KC_LALT, KC_LCTL , KC_SPC , SYM   ,     NAV    , KC_ENT ,KC_RALT, ADJUST, FKEYS
+                                KC_LGUI,  KC_LALT, KC_LCTL , KC_SPC , SYM   ,     NAV    , KC_ENT ,KC_RALT, FKEYS , ADJUST
     ),
 
 /*
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYM] = LAYOUT(
 	    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                                     KC_1, KC_2, KC_3, KC_4, KC_5, KC_DEL,
 	    KC_TAB, KC_6, KC_7, KC_8, KC_9, KC_0,                                                     KC_6, KC_7, KC_8, KC_LPRN, KC_RPRN, KC_PIPE,
-	    KC_TRNS, KC_EQL, KC_MINS, KC_UNDS, KC_LCBR, KC_RCBR, KC_TRNS,_______,       _______, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS,
-	                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, ADJUST, KC_TRNS, KC_TRNS, KC_TRNS
+	    KC_TRNS, KC_EQL, KC_MINS, KC_UNDS, KC_LCBR, KC_RCBR, KC_TRNS,_______,       _______, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_SLSH,
+	                     KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, ADJUST, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 /*
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_FUNCTION] = LAYOUT(
       _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______,
+      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , KC_F9,                                       _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC ,KC_QUOTE,KC_COMM,  KC_DOT,   KC_P ,   KC_Y ,                                        KC_F,   KC_G ,  KC_C ,   KC_R ,  KC_L , KC_BSPC,
      KC_TAB, KC_A ,  KC_O   ,  KC_E  ,   KC_U ,   KC_I ,                                        KC_D,   KC_H ,  KC_T ,   KC_N ,  KC_S  , KC_MINS,
      KC_LSFT ,KC_SCLN, KC_Q   ,  KC_J  ,   KC_K ,   KC_X , KC_LBRC,KC_MUTE,     _______, KC_RBRC, KC_B,   KC_M ,  KC_W ,   KC_V ,  KC_Z , KC_RSFT,
-                                 KC_LGUI, KC_LALT, KC_LCTL, KC_SPC , NAV   ,     NAV    , KC_ENT ,KC_RALT, ADJUST, FKEYS
+                                KC_LGUI,  KC_LALT, KC_LCTL , KC_SPC , SYM   ,     NAV    , KC_ENT ,KC_RALT, FKEYS , ADJUST
     ),
 
 };
--- readme.md
+++ readme.md
@@ -6,5 +6,7 @@
 $ mkdir -p keyboards/splitkb/aurora/sofle_v2/keymaps/personal
 $ cp * <qmk_firware_dir>/keyboards/splitkb/aurora/sofle_v2/keymaps/personal/
 $ qmk compile -kb splitkb/aurora/sofle_v2/rev1 -km personal -e CONVERT_TO=liatris
+$ $ qmk compile -kb splitkb/aurora/sofle_v2/rev1 -km personal -e CONVERT_TO=liatris
+
 ```
 
