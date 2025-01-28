6f545ceba66b4ae9849b7570116f841e2f522575
yottta <andreic9203@gmail.com>
2025-01-28 10:09:04 +0200

    sofle switch base layer to the macos one
    Signed-off-by: yottta <andreic9203@gmail.com>

--- keyboards/splitkb/aurora/sofle_v2/rev1/keymaps/personal/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/rev1/keymaps/personal/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _WIN = 0,
-    _MAC,
+    _MAC = 0,
+    _WIN,
     _SYM,
     _NAV,
     _FUNCTIONS,
@@ -14,6 +14,13 @@ enum layers {
 #define SWITCH_TO_MAC   DF(_MAC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_MAC] = LAYOUT(
+	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, /* ---- */ KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
+	KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, /* ---- */ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+	KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, /* ---- */ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+	KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, /* ---- */ KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+	KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_SYM), /* ---- */ MO(_NAV), KC_ENT, KC_RCTL, KC_RALT, MO(_FUNCTIONS)
+    ),
     [_WIN] = LAYOUT(
 	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, /* ---- */ KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
 	KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, /* ---- */ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
@@ -21,14 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, /* ---- */ KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
 	KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, MO(_SYM), /* ---- */ MO(_NAV), KC_ENT, KC_RCTL, KC_RALT, MO(_FUNCTIONS)
     ),
-
-    [_MAC] = LAYOUT(
-	KC_ESC, KC_2, KC_1, KC_3, KC_4, KC_5, /* ---- */ KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
-	KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, /* ---- */ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-	KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, /* ---- */ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-	KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, /* ---- */ KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-	KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_SYM), /* ---- */ MO(_NAV), KC_ENT, KC_RCTL, KC_RALT, MO(_FUNCTIONS)
-    ),
+ //    [_SYM] = LAYOUT(
+	// KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /* ---- */ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+	// KC_ESC, KC_NO, KC_NO, KC_4, KC_5, KC_6, /* ---- */ KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_DEL,
+	// KC_TRNS, KC_NO, KC_0, KC_1, KC_2, KC_3, /* ---- */ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+	// KC_TRNS, KC_EQL, KC_MINS, KC_7, KC_8, KC_9, KC_TRNS, /* ---- */ KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS,
+	// KC_TRNS, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, /* ---- */ MO(_ADJUST), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+ //    ),
     [_SYM] = LAYOUT(
 	KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /* ---- */ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, /* ---- */ KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_DEL,
